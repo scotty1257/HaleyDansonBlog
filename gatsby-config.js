39267f46f7848777fba7350d1c35ebc1094b17cf
JDS28 <JDS28@DZ4-JDS28-DSA>
2022-10-05 10:05:58 -0400

    Style Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Haley Danson",
+    description: "A Blog by Haley Danson | My Life",
   },
   plugins: [
     "gatsby-transformer-sharp",
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Haley Danson | HKD</span>
+      <span className={styles.navigationItem}>Haley Danson</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
